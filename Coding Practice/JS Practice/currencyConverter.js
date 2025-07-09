f234ba839f4931012cf2c67452edb13bad7e0b87
ktubbs24 <kwenelatubbs@pm.me>
2025-07-08 22:32:50 -0700

    Create currencyConverter.js
    practiced some javascript variable and declaring with this exercise from the video of Iman Musa

---
+++ Coding Practice/JS Practice/currencyConverter.js
@@ -0,0 +1,22 @@
+// Currency Converter 
+
+// I want to convert AUD (Australian Dollars) to USD (United States Dollars) using the $AUD to $USD Formula: 
+
+//   AUD = USD * 1.5
+
+
+let USD = 1200; 
+let AUD = 1200 * 1.5;  
+
+console.log("$" + AUD); 
+// The answer is $1800 AUD
+
+/* or it can be written this way to come up with the same answer (I just prefer the first way because it is the same data types on both sides of the equation):
+
+let USD = 1200; 
+let AUD = USD * 1.5;  
+
+console.log("$" + AUD); 
+
+
+*/
